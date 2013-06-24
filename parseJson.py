b064f2221ece24b541a35d20712af155a42ce693
RuthRainbow <ruth.king1991@gmail.com>
2013-06-25 00:09:09 +0100

    Python to parse json and print top 6 game

---
+++ parseJson.py
@@ -0,0 +1,27 @@
+import json
+import operator
+from pprint import pprint
+
+with open('data.json') as data_file:    
+  data = json.load(data_file)
+
+gamesTable = {}
+
+for x in data["Users"]:
+  for y in x["wantToPlay"]:
+    if y["name"] in gamesTable:
+        gamesTable[y["name"]] += 1
+    else:
+        gamesTable[y["name"]] = 1	
+
+maxDisplay = 6
+sortedGames = sorted(gamesTable.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+#then just take the first 6 from this sortedGames list :)))
+
+count=0
+
+for popularity, game in enumerate(sortedGames):
+  if count < maxDisplay:
+    pprint(game)
+
