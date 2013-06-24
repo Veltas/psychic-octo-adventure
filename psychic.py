144b9565de10b278696e5e21e564945eefb50b8d
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-06-24 23:53:29 +0100

    Free is not a unique field

--- psychic.py
+++ psychic.py
@@ -29,7 +29,7 @@ class User(db.Model):
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Integer, unique=True)
-    free = db.Column(db.Boolean, unique=True)
+    free = db.Column(db.Boolean)
 
     def __init__(self, name, free=False):
         self.name = name
